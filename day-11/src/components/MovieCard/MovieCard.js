176bb37061c12b96e58c5236cad47253fff8aa01
lulzphantom <caarteagag@gmail.com>
2020-02-06 17:49:41 -0500

    day 11 react update

--- day-11/src/components/MovieCard/MovieCard.js
+++ day-11/src/components/MovieCard/MovieCard.js
@@ -1,24 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './MovieCard.css';
 
 export default class MovieCard extends Component {
+
+    movieCards = () => {
+        let movies = [];
+
+        for (let i = 0; i < 10; i++) {
+            movies.push(
+                <div className="col s12 m2">
+                        <div className="card">
+                            <div className="card-image">
+                                <img src={this.props.image} alt="movie"/>
+                                <span className="card-title">{this.props.title}</span>
+                            </div>
+                            <div className="card-content">
+                                <p> {this.props.description}</p>                       
+                            </div>
+                            <div className="card-action">
+                                <p> {this.props.director}</p>
+                                <p> {this.props.theme}</p>
+                            </div>
+                        </div>
+                </div> 
+            );
+        }
+
+        return movies;
+    }
+
     render() {
         return (
             <div>
-                <div className="col s12 m3">
-                    <div class="card">
-                    <div class="card-image">
-                        <img src={this.props.image} alt="movie"/>
-                        <span class="card-title">{this.props.title}</span>
-                    </div>
-                    <div class="card-content">
-                        <p> {this.props.description}</p>                       
-                    </div>
-                    <div class="card-action">
-                        <p> {this.props.director}</p>
-                        <p> {this.props.theme}</p>
-                    </div>
-                    </div>
-                </div>
+                {this.movieCards()}
             </div>
         )
     }
--- day-11/src/containers/movies/MoviesContainer.js
+++ day-11/src/containers/movies/MoviesContainer.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import MovieCard from '../../components/MovieCard/MovieCard'
+import requestPromise from 'request-promise'
 
 export const MoviesContainer = () => {
+
+    //Get movies from API
+    
+    const getMoviesPromise = () => {
+        var options = {
+            method: 'GET',
+            uri: 'https://devf-movies.herokuapp.com/api/v1/getMovies',          
+            json: true        
+        };        
+        return requestPromise(options).promise();
+    }
+
+    const movies = () => {        
+        console.log('test');
+        getMoviesPromise()
+            .then((result) => {
+                return <h1>{result}</h1>
+            }).catch((err) => {
+                console.log(err);
+                return <h2>{err}</h2>
+            });
+    }
+
     return (
         <div>
             <h1>Contenedor de peliculas</h1>
             <section>
                 <div className="row">
+                    {movies()}
                     <MovieCard 
                         image="https://pbs.twimg.com/media/EPAPGw8WAAAgDQs.jpg"
                         title="Devf"
